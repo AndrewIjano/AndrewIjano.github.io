12b639b6607144a04dca26fb16b45473e61c7aff
Andrew Ijano <andrew.ijano@gmail.com>
2020-08-13 16:05:56 -0300

    deploy

---
+++ precache-manifest.d653025bba263d4119a279dd8efbf8bd.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80397c2bb23efc6577989db14a4625ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "952d2b1dfb6308fb9909",
+    "url": "/static/css/main.1d865a27.chunk.css"
+  },
+  {
+    "revision": "1714cb80152d3daa7d1c",
+    "url": "/static/js/2.91fc7112.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.91fc7112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "952d2b1dfb6308fb9909",
+    "url": "/static/js/main.1c9e379b.chunk.js"
+  },
+  {
+    "revision": "87907377d2dd5fa6a303",
+    "url": "/static/js/runtime-main.4e37241f.js"
+  },
+  {
+    "revision": "ae3bd0f661ccee416273788d6aad6614",
+    "url": "/static/media/alura.ae3bd0f6.svg"
+  },
+  {
+    "revision": "943341db5f4e2bf850b6aabc276e8a39",
+    "url": "/static/media/brid.943341db.svg"
+  },
+  {
+    "revision": "cfcdb77252b5a944a0a48aa6fe21db21",
+    "url": "/static/media/github.cfcdb772.svg"
+  },
+  {
+    "revision": "a0d16f1099643424529cd6aacd715d2b",
+    "url": "/static/media/hacksbseg.a0d16f10.svg"
+  },
+  {
+    "revision": "06b26603d493fb5e3ed9e292b7b451ef",
+    "url": "/static/media/ijanolopes.06b26603.svg"
+  },
+  {
+    "revision": "273ba60df23ceb21bb2b02e40a94248f",
+    "url": "/static/media/imesec.273ba60d.svg"
+  },
+  {
+    "revision": "81ba6c692ea08f398363c041554eac77",
+    "url": "/static/media/instagram.81ba6c69.svg"
+  },
+  {
+    "revision": "0ab67ece4625d24d249477137d0da1b5",
+    "url": "/static/media/lh.0ab67ece.svg"
+  },
+  {
+    "revision": "cd82663b0c843551693c92e4c7b6d292",
+    "url": "/static/media/linkedin.cd82663b.svg"
+  },
+  {
+    "revision": "2a12e64c6d2e5314d3ec4f018248e9f9",
+    "url": "/static/media/pgelsat.2a12e64c.svg"
+  },
+  {
+    "revision": "46b7d9b69432b174e3106b456cba7258",
+    "url": "/static/media/regex.46b7d9b6.svg"
+  }
+]);
